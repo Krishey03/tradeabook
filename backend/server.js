d3b3017f99fc62075f097c3abc2a542254ddfa97
Krish Bhattarai <112310797+Krishey03@users.noreply.github.com>
2024-12-29 13:09:28 +0545

    first commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect('mongodb+srv://dbuser:test123@cluster0.nwtjx.mongodb.net/')
+    .then(() => console.log('MongoDB Connected'))
+    .catch((error) => console.log(error));
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST", "DELETE", "PUT"],
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma",
+         ],
+        credentials: true,
+    })
+)    
+    
+app.use(cookieParser())
+app.use(express.json())
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
