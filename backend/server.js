4239870a07adaa69827513ce574a711e3389edb8
Krish Bhattarai <112310797+Krishey03@users.noreply.github.com>
2025-06-06 08:13:14 +0545

    backend change

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+          origin: [
+            "http://localhost:5173",
+            "https://tradeabook-production.up.railway.app",
+          ],
         methods: ["GET", "POST"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
